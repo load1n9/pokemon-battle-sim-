3eab6fb017c56ced7daef44b72bc8450aecfd381
loading <49134864+load1n9@users.noreply.github.com>
2020-10-28 10:18:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import random
 
 class Battle:
     def __init__(self, a,b):
+      self.data = {
+          "pokemon": "test",
+          "msg": []
+      }
       self.setStats(a,b)
       self.setMoves()
 
@@ -23,23 +27,24 @@ class Battle:
       self.p2["move"] = pb.pokemon(self.p2["name"]).moves[p2int].move.name
       self.attack()
     def attack(self):
-      if int(self.p1["health"]) and int(self.p2["health"]) > 0:
+      if int(self.p1["health"]) > 0 and int(self.p2["health"]) > 0:
         try:  
-          print(self.p1["name"]," uses ", self.p1["move"])
+          self.data["msg"].append(self.p1["name"]+" uses "+self.p1["move"])
           self.p1["health"] -= int(pb.move(str(self.p2["move"])).power)
-          print(self.p2["name"]," uses ", self.p2["move"])
+          self.data["msg"].append(self.p2["name"]+" uses "+self.p2["move"])
           self.p2["health"] -= int(pb.move(str(self.p1["move"])).power)
-          print(self.p1["name"],": ",self.p1["health"])
-          print(self.p2["name"],": ",self.p2["health"])
+          self.data["msg"].append(self.p1["name"]+": "+str(self.p1["health"]))
+          self.data["msg"].append(self.p2["name"]+": "+str(self.p2["health"]))
           self.setMoves()
         except:
           self.setMoves()
       elif self.p1["health"] <= 0:
-        print(pb.pokemon(self.p1["name"])," is unable to move")
+        self.data["pokemon"] = pb.pokemon(self.p2["name"])
       elif self.p2["health"] <= 0:
-        print(pb.pokemon(self.p2["name"])," is unable to move")
+        self.data["pokemon"] = pb.pokemon(self.p1["name"])
        
 
 
 
-test = Battle(34,56)
+test = Battle(134,356)
+print(test.data["msg"])
