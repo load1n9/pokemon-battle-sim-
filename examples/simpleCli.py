e8bcd06121db009d91154016ece1e5182fda4190
loading <49134864+load1n9@users.noreply.github.com>
2020-10-28 10:31:53 -0400

    Create simpleCli.py

---
+++ examples/simpleCli.py
@@ -0,0 +1,10 @@
+from ../main.py import Battle
+try:
+  a = int(input("first pokemon id: "))
+  b = int(input("second pokemon id: "))
+  test = Battle(a,b)
+  for x in test.data["msg"]:
+     print(x)
+  print(test.data["pokemon"]," won")
+except:
+  print("something went wrong")
